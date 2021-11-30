7e64abe8437b432790145c06d13c7435e8009764
N-nec <95116319+N-nec@users.noreply.github.com>
2021-11-30 01:04:42 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-dofile(minetest.get_modpath("liquid").."/liquids.lua")
+dofile(minetest.get_modpath("liquids").."/liquids.lua")
 
 if minetest.setting_get("log_mods") then
 	minetest.log("action", "[MOD] Liquids has loaded")
